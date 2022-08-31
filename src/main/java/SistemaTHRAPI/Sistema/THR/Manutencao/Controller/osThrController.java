02aa76d659c357f60e0db66d18e9bd79a2c67b82
vitor <vitorhugorodrigues93@gmail.com>
2022-08-31 09:14:50 -0300

    controller

--- src/main/java/SistemaTHRAPI/Sistema/THR/Manutencao/Controller/osThrController.java
+++ src/main/java/SistemaTHRAPI/Sistema/THR/Manutencao/Controller/osThrController.java
@@ -23,7 +23,7 @@ import SistemaTHRAPI.Sistema.THR.Manutencao.Service.osThrService;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200/maintenance https://app-thr.herokuapp.com/maintenance")
+@CrossOrigin(origins = "http://localhost:4200/maintenance")
 
 @RequestMapping("/Ordem-Servico")
 
